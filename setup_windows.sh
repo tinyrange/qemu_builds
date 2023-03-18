b8f94538a18e60c5caa89b1dd21563d726d07b1b
Joshua Scarsbrook <jscarsbrook@gmail.com>
2023-03-18 12:11:07 +1000

    add noconfirm to pacman for Windows

--- setup_windows.sh
+++ setup_windows.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-pacman -S base-devel mingw-w64-x86_64-toolchain git python ninja
-pacman -S mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman python-setuptools
+pacman --noconfirm -S base-devel mingw-w64-x86_64-toolchain git python ninja
+pacman --noconfirm -S mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman python-setuptools
