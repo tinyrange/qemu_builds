3a9af37b98755a3685094cb1b4718b9eb68e17d6
Joshua Scarsbrook <jscarsbrook@gmail.com>
2023-03-18 12:39:10 +1000

    I wonder if vhost-user works on Windows

--- build_windows.sh
+++ build_windows.sh
@@ -9,5 +9,7 @@ QEMU=$PWD/qemu
 mkdir -p $BUILD
 mkdir -p $DIST
 
+cp patches/meson.build qemu/meson.build
+
 (cd $BUILD; $QEMU/configure --without-default-features --target-list=x86_64-softmmu --enable-vhost-user --static --prefix=$DIST)
 (cd $BUILD; ninja install)
