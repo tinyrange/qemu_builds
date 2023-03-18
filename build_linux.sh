de3bc7d6b8a0d827ceeebb841f2e6e5a1de3ecce
Joshua Scarsbrook <jscarsbrook@gmail.com>
2023-03-18 11:23:18 +1000

    Added build scripts for Windows and Linux

---
+++ build_linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+DIST=$PWD/dist
+BUILD=$PWD/build
+QEMU=$PWD/qemu
+
+mkdir -p $BUILD
+mkdir -p $DIST
+
+(cd $BUILD; $QEMU/configure --without-default-features --target-list=x86_64-softmmu --enable-vhost-user --prefix=$DIST)
+(cd $BUILD; ninja install)
